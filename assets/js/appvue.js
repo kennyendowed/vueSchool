5dee0cb0431d5b99be7b7d8c36f9a1e58d6221e2
Kenneth Akpan <kennetha@spearheadconsult.com>
2021-06-08 11:26:39 +0100

    first commint

---
+++ assets/js/appvue.js
@@ -0,0 +1,11 @@
+// when life is settled, load up the fun stuff
+document.addEventListener('DOMContentLoaded', function () {
+  new Vue({
+    el: '#applist',
+    // define data - initial display text
+    data: {
+      m1: "You got to let me know",
+      message:"first class with vue"
+    }
+  })
+})
